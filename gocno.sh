c95ddf5d9e346578846cdca87b34c3b7c1b535cf
фанинажопа <faninpop18@gmail.com>
2026-04-20 12:23:21 +0000

    Update gocno.sh

--- gocno.sh
+++ gocno.sh
@@ -2,5 +2,10 @@
 set -e
 sudo pacman -Syu
 sudo pacman -S  hyprland uwsm waybar swww fastfetch ttf-jetbrains-mono-nerd pavucontrol kitty rofi
+rm -rf ~/.config/hypr
+rm -rf ~/.config/fastfetch
+rm -rf ~/.config/rofi
+rm -rf ~/.config/kitty
+
 cp -r hypr kitty fastfetch rofi ~/.config/
 awww img k2af87zzsvze1.jpeg
