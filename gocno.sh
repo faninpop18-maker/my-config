11a95d7c29d6f8ff14ce1eb37184bd3f9f59b730
фанинажопа <faninpop18@gmail.com>
2026-06-28 21:44:52 +0500

    Update gocno.sh

--- gocno.sh
+++ gocno.sh
@@ -3,7 +3,7 @@ set -e
 cd "$(dirname "$0")"
 sudo pacman -Syyu
 sudo pacman -S --noconfirm hyprland uwsm waybar awww fastfetch brightnessctl \
-ttf-jetbrains-mono-nerd pavucontrol kitty rofi noto-fonts noto-fonts-emoji \
+ttf-jetbrains-mono-nerd pavucontrol kitty rofi noto-fonts noto-fonts-emoji rofi-emoji \
 pipewire pipewire-pulse pipewire-jack pipewire-alsa ark \
 wireplumber pipewire-alsa hyprshot nerd-fonts swaync dolphin
 rm -rf ~/.config/hypr
